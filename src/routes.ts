fc0a0db09136958d22fd34c93acd9b71a7293c59
David Amorim <davidfilipeamorim@gmail.com>
2021-04-22 16:22:00 +0100

    Add MessagesController

--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,17 @@ import { Router } from 'express';
 
 import SettingsController from './controllers/SettingsController';
 import UsersController from './controllers/UsersController';
+import MessagesController from './controllers/MessagesController';
 
 const settingsController = new SettingsController();
 const usersController = new UsersController();
+const messagesController = new MessagesController();
 
 const routes = Router();
 
 routes.post('/settings', settingsController.create);
 routes.post('/users', usersController.create);
+routes.post('/messages', messagesController.create);
+routes.get('/messages/:id', messagesController.showByUser);
 
 export default routes;
