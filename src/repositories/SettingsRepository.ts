0391ef580db22b490c8bac4fb22dc87d49844e7c
David Amorim <davidfilipeamorim@gmail.com>
2021-04-22 15:27:14 +0100

    Add settings route

---
+++ src/repositories/SettingsRepository.ts
@@ -0,0 +1,6 @@
+import { EntityRepository, Repository } from "typeorm";
+
+import { Setting } from "../entities/Setting";
+
+@EntityRepository(Setting)
+export class SettingsRepository extends Repository<Setting> {}
---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+import SettingsController from './controllers/SettingsController';
+
+const settingsController = new SettingsController();
+
+const routes = Router();
+
+routes.post('/settings', settingsController.create);
+
+export default routes;
