9589cdf6293c75e3dbf0eb0dbc966c47543fe3ab
David Amorim <davidfilipeamorim@gmail.com>
2021-04-22 15:54:10 +0100

    Add UsersController

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import SettingsController from './controllers/SettingsController';
+import UsersController from './controllers/UsersController';
 
 const settingsController = new SettingsController();
+const usersController = new UsersController();
 
 const routes = Router();
 
 routes.post('/settings', settingsController.create);
+routes.post('/users', usersController.create);
 
 export default routes;
