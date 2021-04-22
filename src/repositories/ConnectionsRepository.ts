e3b161d4b06568048a85281b2c082819f7355335
David Amorim <davidfilipeamorim@gmail.com>
2021-04-22 17:26:30 +0100

    Add ConnectionsRepository

---
+++ src/repositories/ConnectionsRepository.ts
@@ -0,0 +1,6 @@
+import { EntityRepository, Repository } from "typeorm";
+
+import { Connection } from '../entities/Connection';
+
+@EntityRepository(Connection)
+export class ConnectionsRepository extends Repository<Connection> {}
